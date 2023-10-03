516f2ac3a95b6896f90a7f2abf88c21a2d156785
DerputeR <dekxy-developer@yahoo.com>
2023-10-03 19:37:48 -0400

    Began work on location searchbar front-end

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInPage from "./components/sign-in/SignIn";
 import SignUpPage from "./components/sign-up/SignUp";
 import LandingPage from "./components/landing/Landing";
+import ComponentTestPage from "./components/test/ComponentTest";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="landing" element={<LandingPage />} />
         <Route path="" element={<SignInPage />} />
         <Route path="sign-up" element={<SignUpPage />} />
+        <Route path="test" element={<ComponentTestPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/test/ComponentTest.js
@@ -0,0 +1,9 @@
+import LocationSearch from "../inputs/LocationSearch";
+
+function ComponentTestPage() {
+    return (
+      <LocationSearch />
+    );
+  }
+  
+  export default ComponentTestPage;
