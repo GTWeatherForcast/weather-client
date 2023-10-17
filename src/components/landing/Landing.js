804b8f3cb44e0f32423bfccd301e4eb1680da401
DerputeR <dekxy-developer@yahoo.com>
2023-10-17 17:44:47 -0400

    Attempting to make landing's css only apply to itself. Not working yet

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,4 +1,4 @@
-import styles from "./Landing.css";
+import styles from "./Landing.module.css";
 
 function LandingPage() {
   const signInClick = () => {};
@@ -7,14 +7,14 @@ function LandingPage() {
     <div>
       <nav>
         <ul>
-          <li className="sign">
-            <a className="navBarText textstyle" onClick={() => signInClick}>
+          <li className={styles.sign}>
+            <a className={`${styles.navBarText} ${styles.textstyle}`} onClick={() => signInClick}>
               sign in
             </a>
           </li>
 
-          <li className="sign">
-            <a className="sign navBarText textstyle" onClick={() => signUpClick}>
+          <li className={styles.sign}>
+            <a className={`${styles.sign} ${styles.navBarText} ${styles.textstyle}`} onClick={() => signUpClick}>
               sign up
             </a>
           </li>
@@ -26,13 +26,13 @@ function LandingPage() {
 
       </a> */}
 
-      <div id="content-container">
-        <p id="welcome" className="center textstyle">
+      <div id={styles["content-container"]}>
+        <p id={styles["welcome"]} className={`${styles.center} ${styles.textstyle}`}>
           Welcome to Georgia Tech Weather Forecast!
         </p>
 
-        <div className="center">
-          <div id="rectangle"></div>
+        <div className={styles.center}>
+          <div id={styles["rectangle"]}></div>
         </div>
       </div>
     </div>
