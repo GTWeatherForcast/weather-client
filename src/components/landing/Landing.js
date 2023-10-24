760608bf5eb7507bfea195639b205cc8948c1238
DerputeR <dekxy-developer@yahoo.com>
2023-10-24 00:04:58 -0400

    Added border transition for signin/up cards

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,29 +1,42 @@
 import styles from "./Landing.module.css";
 import { LocationSearch } from "../inputs/SearchBar"
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 export default function LandingPage() {
   return (
-    <div className={`${styles.landingBody}`}>
-      <nav className={`${styles.mainNav} ${styles.textStyle}`}>
-        <ol>
-          <li><a href=".">gt forecast</a></li>
-        </ol>
-        <ol>
-          <li><a href="./login">sign in</a></li>
-          <li><a href="./signup">sign up</a></li>
-        </ol>
-      </nav>
+    <div className={styles.landingRoot}>
+      {/* <div className={styles.cardLeft}>
+        left card
+      </div> */}
 
-      <div id={styles["content-container"]}>
-        <p id={styles["welcome"]} className={`${styles.center} ${styles.textStyle}`}>
-          Welcome to Georgia Tech Weather Forecast!
-        </p>
-        <div id={styles["rectangle"]}></div>
-        <LocationSearch />
+      {/* ${styles.shiftForRight} ${styles.shiftForLeft} */}
+      <div className={`${styles.landingBody} ${styles.shiftForRight}`}>
+        <div className={styles.landingNav}>
+          <nav className={`${styles.mainNav} ${styles.textStyle}`}>
+            <ol>
+              <li><Link to=".">gt forecast</Link></li>
+            </ol>
+            <ol>
+              <li><Link to="./login">sign in</Link></li>
+              <li><Link to="./signup">sign up</Link></li>
+            </ol>
+          </nav>
+        </div>
+
+        <div id={styles["content-container"]}>
+          <p id={styles["welcome"]} className={`${styles.center} ${styles.textStyle}`}>
+            Welcome to Georgia Tech Weather Forecast!
+          </p>
+          <div id={styles["rectangle"]}></div>
+          <LocationSearch />
+        </div>
+        {/* todo: change classes to reflect whether it is signup or login page so we can move stuff around */}
+        <Outlet />
       </div>
-      {/* todo: change classes to reflect whether it is signup or login page so we can move stuff around */}
-      <Outlet />
+
+      <div className={styles.cardRight}>
+        right card
+      </div>
     </div>
   );
 }
