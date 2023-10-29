0bfd49c415205b1b72eb4fe720fba71d5304848f
DerputeR <dekxy-developer@yahoo.com>
2023-10-29 01:14:50 -0400

    Signup form components separated

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -1,97 +1,65 @@
 import styles from '../inputs/FormInput.module.css';
 import "../../App.css";
-import { useState } from 'react';
 import FormInput from '../inputs/FormInput';
 
-function SignUpPage() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
+function noAction(e) {
+  try {
+    e.preventDefault();
+  } catch { }
+}
 
+// todo: form submission
+// commented this line out since it's a complicated mess for
+// ending up only having one way to submit this form at the end of the day
+// function SignUpPage({ onSubmit = (e) => {noAction(e)} }) {
+function SignUpPage() {
+  // todo: still need to do server-side form validation
   return (
     <div className="cardRoot">
       <h2 className="centerText">SIGN UP FOR FREE</h2>
       <p className="centerText">We won't bite, we promise</p>
 
-      <form action="" className={`${styles.signupForm}`} onSubmit={(e) => { e.preventDefault(); }}>
+      {/* This line was meant to be used with the commented-out function header */}
+      {/* <form action="" className={`${styles.signupForm}`} onSubmit={(e) => { onSubmit(e) }}> */}
+      <form action="" className={`${styles.signupForm}`} onSubmit={(e) => { e.preventDefault() }}>
         
-        <FormInput id={"test"} label={"test label"} placeholder={"does this thing work?"} type={"text"} startValue={"hey!"}/>
+        <FormInput
+          type="text"
+          id="username"
+          label="Username"
+          placeholder=""
+        />
 
-        <div className={`${styles.inputDiv}`}>
-          <label for="username">Username</label>
-          <input
-            id="username"
-            className={`${styles.textInput}`}
-            type="text"
-            data-label="Username"
-            placeholder=""
-            // value={username}
-            // onChange={(e) => { setUsername(e.target.value); }}
-            // onSubmit={(e) => { e.preventDefault(); }}
-          />
-        </div>
+        <FormInput
+          type="email"
+          id="email"
+          label="Email"
+          placeholder=""
+          // onClick={(e) => { console.log(`Email pattern: ${e.target.pattern}`)} }
+        />
 
-        <div className={`${styles.inputDiv}`}>
-          <label for="email">Email</label>
-          <input
-            id="email"
-            className={`${styles.textInput}`}
-            type="text"
-            data-label="Email"
-            placeholder=""
-            // value={email}
-            // onChange={(e) => { setEmail(e.target.value); }}
-            // onSubmit={(e) => { e.preventDefault(); }}
-          />  
-        </div>
+        <FormInput
+          type="password"
+          id="password"
+          label="Password"
+          placeholder=""
+          title="Password requirements: ?"
 
-        <div className={`${styles.inputDiv}`}>
-          <label for="password">Password</label>
-          <input
-            id="password"
-            className={`${styles.textInput}`}
-            type="password"
-            data-label="Password"
-            placeholder=""
-            // value={password}
-            // onChange={(e) => { setPassword(e.target.value); }}
-            // onSubmit={(e) => { e.preventDefault(); }}
-          /> 
-        </div>
+        />
 
-        <div className={`${styles.inputDiv}`}>
-          <label for="password2">Confirm Password</label>
-          <input
-            id="password2"
-            className={`${styles.textInput}`}
-            type="password"
-            data-label="Password"
-            placeholder=""
-            // value={password2}
-            // onChange={(e) => { setPassword2(e.target.value); }}
-            // onSubmit={(e) => { e.preventDefault(); }}
-          /> 
-        </div>
+        <FormInput
+          type="password"
+          id="password2"
+          label="Confirm Password"
+          placeholder=""
+          title="Please reenter your password."
+        />
 
-        <div className={`${styles.inputDiv} ${styles.submitContainer}`}>
-          <input
-            id="signup"
-            className={`${styles.textInput}`}
-            type="submit"
-            value="Sign Up"
-            // onChange={(e) => { e.preventDefault(); }}
-            // onSubmit={(e) => { e.preventDefault(); }}
-          /> 
-        </div>
-
-        
-
-        
-
-
-
-
+        <FormInput
+          type="submit"
+          id="signup"
+          value="Sign Up"
+        />
       </form>
     </div>
   );
