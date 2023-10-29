c1c68393b54a16806aa0ca48bf0c7b096e9f8b43
DerputeR <dekxy-developer@yahoo.com>
2023-10-29 01:32:04 -0400

    Login page initial styling

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,51 +1,37 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { LandingCard } from "../landing/Landing";
-import stylesLanding from '../landing/Landing.module.css';
-import styles from "./SignIn.css";
+import styles from '../inputs/FormInput.module.css';
+import "../../App.css";
+import FormInput from '../inputs/FormInput';
 
+// todo: form submission
 export default function SignInPage() {
-  const [messageFromServer, setMessageFromServer] = useState(undefined);
-
-  useEffect(() => {
-    // dummy example of how to make a request to our server
-    // note: make sure the server is running on your machine
-    const API_URL = "http://localhost:8080";
-    axios
-      .get(API_URL)
-      .then((response) => {
-        const serverMessage =
-          response?.data?.message || "no message from server";
-        setMessageFromServer(
-          `Got this message from our server: ${serverMessage}`
-        );
-      })
-      .catch((error) => {
-        setMessageFromServer("Error communicating with server");
-        console.error("Error communicating with server", error);
-      });
-  }, []);
-
-  const testNow = () => {
-    axios.post('http://localhost:8080/api/v1/sign-in', {
-      message: 'working'
-    }).then((response) => {
-      console.log(response.data.message)
-    }).catch((error) => {
-      console.error('Error communicating with server', error);
-    });
-  }
-
+  // todo: still need to do server-side form validation
   return (
-    <div>
-      <p className={styles.dummyStyle}>Sign in page</p>
-      <p>{messageFromServer || 'communicating with server...'}</p>
-      <a href='/'>Back</a>
-      <button onClick={testNow}>Test</button>
-      <p className={styles.dummyStyle}>Sign in page</p>
-      <p id="logo">gt forecast</p>
-      <p>{messageFromServer || "communicating with server..."}</p>
-      <a href="/">Back</a>
+    <div className="cardRoot">
+      <h2 className="centerText">LOGIN</h2>
+      <p className="centerText">Welcome back!</p>
+
+      <form action="" className={`${styles.mainForm}`} onSubmit={(e) => { e.preventDefault() }}>
+        
+        <FormInput
+          type="text"
+          id="username"
+          label="Username"
+          placeholder=""
+        />
+
+        <FormInput
+          type="password"
+          id="password"
+          label="Password"
+          placeholder=""
+        />
+
+        <FormInput
+          type="submit"
+          id="signup"
+          value="Sign In"
+        />
+      </form>
     </div>
   );
 }
